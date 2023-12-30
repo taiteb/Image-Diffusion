63167ffaf86cf18654e332cec05db07f5ef70546
taiteb <taiteb@gmail.com>
2023-12-29 20:00:43 -0700

    init: template p5 sketch w/ concept in comments

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// In this sketch I want to combine two previous projects:
+// I want to use the image processing elements of my ASCII project
+// to seed a reaction diffusion gradient. I'll have to scale back
+// the number of options outputted from the ASCII processing
+// to a simple binary; if the image is light, give it A, if it's
+// dark, give it B. 
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
